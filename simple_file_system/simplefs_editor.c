9ea560d6eecd3b24659c49d89709910bd2ff869f
Gabriele Mottola <mottola.1874718@studenti.uniroma1.it>
2020-05-14 14:12:06 +0200

    Little initial improvements to repo.

---
+++ simple_file_system/simplefs_editor.c
@@ -0,0 +1,6 @@
+#pragma once
+#include "bitmap.h"
+#include "disk_driver.h"
+#include "simplefs.h"
+
+//This executable will implement high-level operations, like fs browsing and format and a shell.
